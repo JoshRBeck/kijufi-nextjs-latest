7a965abfb7980f41dcc6bb71096bb42435a43717
beckjosh <Joshbeck17@gmail.com>
2023-09-21 01:21:13 +0200

    "Added contact info"

--- src/app/kontakt/page.tsx
+++ src/app/kontakt/page.tsx
@@ -5,6 +5,10 @@ import Image from "next/image";
 import filmTheater from "@/images/filmtheateramfriedrichshain.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import Instagram from "@/images/Instagram.png";
+import Facebook from "@/images/Facebook.png";
+import Youtube from "@/images/Youtube.png";
+import M from "@/images/M.png";
 declare const google: any;
 
 export default function Kontakt() {
@@ -59,7 +63,6 @@ export default function Kontakt() {
           </p>
         </div>
       </div>
-
       {/* Box for Festival */}
       <div className={styles.box}>
         <h1 className={styles.boxTitle}>Für das Festival!</h1>
@@ -77,9 +80,32 @@ export default function Kontakt() {
         </p>
       </div>
       <div className={styles.mapContainer}>
-       {/* Map box */}
-      <Map />
-      </div>       
+        {/* Map box */}
+        <Map />
+      </div>
+      {/* Container for Socials */}
+      <div className={styles.socialsContainer}>
+        <h1 className={styles.kontaktSocialsKijufi}>Kijufi</h1>
+        Landesverband Kinder- und Jugendfilm Berlin e.V. (kijufi)
+        <br />
+        Am Sudhaus 21
+        <br />
+        2053 Berlin
+        <br />
+        Telefon: +49 (0)157 35 70 58 73
+        <br />
+        E-Mail: info@kijufi.de
+        <div className={styles.footerBottomSocials}>
+          <Image
+            src={Instagram}
+            alt="Instagram logo"
+            className={styles.socialIcon}
+          />
+          <Image src={Facebook} alt="Facebook" className={styles.socialIcon} />
+          <Image src={Youtube} alt="Youtube" className={styles.socialIcon} />
+          <Image src={M} alt="M Social Media" className={styles.socialIcon} />
+        </div>
+      </div>
     </div>
   );
 }
