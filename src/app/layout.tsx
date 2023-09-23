a55a850fdaa7441414e039a514e60abaf97dfa70
beckjosh <Joshbeck17@gmail.com>
2023-09-23 19:38:17 +0200

    "suspense error handling"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import styles from "./page.module.css";
+import { Suspense } from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/navbar";
@@ -19,15 +20,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </Head>
-      <body className={inter.className}>
-        <Navbar />
-        <div className={styles.mainContent}>{children}</div>
-        <Footer />
-      </body>
-    </html>
+    <Suspense fallback={<div>Loading...</div>}>
+      <html lang="en">
+        <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+        </Head>
+        <body className={inter.className}>
+          <Navbar />
+          <div className={styles.mainContent}>{children}</div>
+          <Footer />
+        </body>
+      </html>
+    </Suspense>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 import Chair from "@/images/chair.png";
 import Camera from "@/images/camera.png";
 import HeadingNeu from "../images/heading_neu.png";
-import { useEffect, useState } from "react";
+import { useEffect, useState, Suspense } from "react";
 // import RootLayout from "./layout";
 
 export default function Home() {
@@ -38,42 +38,44 @@ export default function Home() {
   }, []);
 
   return (
-    <main className={styles.main}>
-      <div>
-        <div className={styles.titleContainer}>
-          <h1 className={styles.title}>Gemeinsam Grenzenlos</h1>
-          <br></br>
-          <p className={styles.kinderFestival}>
-            Das Kinderrechte-Filmfestival 2023
-          </p>
-        </div>
-        <div className={styles.tiledContainer}>
-          <div className={styles.topTiles}>
-            <div className={styles.tile1}>
-              <Image src={Chair} alt="chair" className={styles.chairImage} />
+    <Suspense fallback={<div>Loading...</div>}>
+      <main className={styles.main}>
+        <div>
+          <div className={styles.titleContainer}>
+            <h1 className={styles.title}>Gemeinsam Grenzenlos</h1>
+            <br></br>
+            <p className={styles.kinderFestival}>
+              Das Kinderrechte-Filmfestival 2023
+            </p>
+          </div>
+          <div className={styles.tiledContainer}>
+            <div className={styles.topTiles}>
+              <div className={styles.tile1}>
+                <Image src={Chair} alt="chair" className={styles.chairImage} />
+              </div>
+              <div className={styles.tile2}></div>
             </div>
-            <div className={styles.tile2}></div>
+            <div className={styles.tile3}>
+              <Image
+                src={Camera}
+                alt="camera image"
+                className={styles.cameraImage}
+              />
+            </div>
+            <div className={styles.tile4}></div>
           </div>
-          <div className={styles.tile3}>
-            <Image
-              src={Camera}
-              alt="camera image"
-              className={styles.cameraImage}
-            />
+          <Image
+            src={HeadingNeu}
+            alt="New heading"
+            className={styles.headingNeu}
+          />
+        </div>
+        <div className={styles.countdownContainer}>
+          <div id="countdownContainer">
+            <p className={styles.countdownNumbers}>{countdownValue}</p>
           </div>
-          <div className={styles.tile4}></div>
         </div>
-        <Image
-          src={HeadingNeu}
-          alt="New heading"
-          className={styles.headingNeu}
-        />
-      </div>
-      <div className={styles.countdownContainer}>
-        <div id="countdownContainer">
-          <p className={styles.countdownNumbers}>{countdownValue}</p>
-        </div>
-      </div>
-    </main>
+      </main>
+    </Suspense>
   );
 }
